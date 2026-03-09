9bad9d47a8b2366d45c583a61d3e2bf68ae3e0e2
Caique7k <caiquealves7k@gmail.com>
2026-03-09 11:49:34 -0300

    feat: implement authentication module with JWT and bcrypt integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransportModule } from './transport/transport.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TransportModule],
+  imports: [TransportModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/transport/transport.controller.ts
+++ backend/src/transport/transport.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { TransportService } from './transport.service';
+import { JwtAuthGuard } from 'src/auth/dto/jwt-auth.guard';
 import { BoardingDto } from './dto/boarding.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('transport')
 export class TransportController {
   constructor(private readonly transportService: TransportService) {}
-
+  @UseGuards(JwtAuthGuard)
   @Post('boarding')
   register(@Body() dto: BoardingDto) {
     return this.transportService.registerBoarding(dto);
